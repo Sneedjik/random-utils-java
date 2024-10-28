89230255bed1d4e6500f1d6a65b5a5920c503bf2
Sergei Volkov <Sneedjik@gmail.com>
2024-10-28 18:10:00 +0600

    Initial commit

---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+    public static void main(String[] args) {
+        System.out.println(getRandomString(10));
+        System.out.println(getRandomEmail());
+        System.out.println(getRandomAddress());
+        System.out.println(getRandomInt(1, 5));
+    }
+
+    public static String getRandomString(int len) {
+        String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+
+        return sb.toString();
+    }
+
+    public static String getRandomEmail() {
+        return getRandomString(10) + "@mail.ru";
+    }
+
+    public static String getRandomAddress() {
+        return getRandomString(10) + " " + getRandomString(5) + " " + getRandomString(5);
+    }
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public static int getRandomPhone(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+}
